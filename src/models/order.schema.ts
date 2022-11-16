40464b0238f41c3b8b7c5ccc041948bda1bfda82
pranav <pranavofficial404@gmail.com>
2022-11-16 15:51:32 +0530

    models and types created

--- src/models/order.schema.ts
+++ src/models/order.schema.ts
@@ -1,1 +1,27 @@
-import * as mongoose from 'mongoose'
+import * as mongoose from 'mongoose';
+
+export const OrderSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: String,
+      ref: 'User',
+    },
+    totalPrice: {
+      type: Number,
+      default: 0,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+        },
+        quantity: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
--- src/models/product.schema.ts
+++ src/models/product.schema.ts
@@ -1,22 +1,19 @@
 import * as mongoose from 'mongoose';
 
-export const ProductSchema = new mongoose.Schema({
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-
-  title: String,
-
-  description: String,
-
-  image: String,
-
-  price: String,
-
-  product: [
-    {
-      productName: String,
+export const ProductSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
-  ],
-});
+
+    title: String,
+
+    description: String,
+
+    image: String,
+
+    price: String,
+  },
+  { timestamps: true },
+);
